6e0669c9c30d284532f18a98348c572016d2bb70
Dev49.net <info@dev49.net>
2016-02-07 06:01:35 +0100

    added text domain

--- buddypress-first-letter-avatar.php
+++ buddypress-first-letter-avatar.php
@@ -2,6 +2,7 @@
 
 /**
  * Plugin Name: BuddyPress First Letter Avatar
+ * Text Domain: buddypress-first-letter-avatar
  * Plugin URI: http://dev49.net
  * Contributors: Dev49.net, DanielAGW
  * Description: Set custom avatars for BuddyPress users. The avatar will be the first (or any other) letter of the user's name on a colorful background.
